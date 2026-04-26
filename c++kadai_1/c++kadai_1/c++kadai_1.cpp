00f11db7a31045a5decfa462dfad95a371301d9d
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-04-26 23:31:53 +0900

    kadai2
    繰り返し計算できる電卓プログラムを作成してください

--- c++kadai_1/c++kadai_1/c++kadai_1.cpp
+++ c++kadai_1/c++kadai_1/c++kadai_1.cpp
@@ -1,15 +1,70 @@
-﻿// c++kadai_1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-// c++Study.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-#include <stdio.h>
+﻿
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
-int main()
+int main() 
 {
-	
+    int num1,num2;
+    char op;
+    bool loop = true;
+    char y;
+
+    printf("数値１を入力してください:\n");
+    scanf_s("%d", &num1);
+
+    while (loop)
+    {
+        printf("数値２を入力してください:\n");
+        scanf_s("%d", &num2);
+
+        printf("演算子を入力してください (+, -, *, /):\n ");
+        scanf_s(" %c", &op, 1);
+
+        switch (op) {
+        case '+':
+            printf("結果:%d\n", num1 + num2);
+            num1 = num1 + num2;
+            break;
+        case '-':
+            printf("結果:%d\n", num1 - num2);
+            num1 = num1 - num2;
+
+            break;
+        case '*':
+            printf("結果:%d\n", num1 * num2);
+            num1 = num1 * num2;
+
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("結果:%d\n", num1 / num2);
+                num1 = num1 / num2;
+            }
+            else {
+                printf("エラー0で除算はできません\n");
+            }
+            break;
+
+        default: 
+            printf("不正な演算子です\n");
+        }
+
+               printf("継続するならyを終了するならそれ以外を押してください\n");
+               scanf_s(" %c", &y, 1);
+
+               if (y == 'y')
+               {
+
+               }
+               else
+               {
+                   loop = false;
+               }
+        }
+        return 0;
 }
+
+
+
