20da22e087898af098ba621a08da3d51ad5eb089
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-04-27 04:10:57 +0900

    kadai3
    指定した回数だけFizzBuzzを表示するプログラムを作成してください

--- c++kadai_1/c++kadai_1/c++kadai_1.cpp
+++ c++kadai_1/c++kadai_1/c++kadai_1.cpp
@@ -1,8 +1,4 @@
-﻿// c++kadai_1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-// c++Study.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿
 
 #include <stdio.h>
 #include <iostream>
@@ -11,5 +7,54 @@ using namespace std;
 
 int main()
 {
-	
+	int minX, maxX;
+	scanf_s("%d", &minX);
+	scanf_s("%d", &maxX);
+
+	int fizz = 3;
+	int buzz = 5;
+
+	if (minX > 1 && maxX < 1000 && minX < maxX)
+	{
+		for (minX; minX < maxX; minX++)
+		{
+			if (fizz == minX && buzz == minX)
+			{
+				printf("FizzBuzz\n");
+				fizz += 3;
+				buzz += 5;
+			}
+			else if (fizz == minX)
+			{
+				printf("Fizz\n");
+
+				fizz += 3;
+
+			}
+			else if (buzz == minX)
+			{
+				printf("Buzz\n");
+
+				buzz += 5;
+
+			}
+			else
+			{
+				printf("%d\n",minX);
+			}
+		}
+	}
+
+	if (minX < 1 || maxX > 1000)
+	{
+		printf("1未満または１０００以上の数値が入力されました。\n");
+	}
+
+	if (minX > maxX)
+	{
+		printf("開始値が終了値より高い値で設定されました。\n");
+	}
+
+
+
 }
